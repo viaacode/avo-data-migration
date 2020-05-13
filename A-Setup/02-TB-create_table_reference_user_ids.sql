75f1fba81400911cefa40146dc69abe8eaf6acb9
Bart Debunne <bart.debunne@viaa.be>
2020-05-13 07:42:36 +0200

    Use avo2 field name in avo1 table reference_user_ids.

--- A-Setup/02-TB-create_table_reference_user_ids.sql
+++ A-Setup/02-TB-create_table_reference_user_ids.sql
@@ -3,7 +3,7 @@
 -- Create table for future mapping of Drupal userid's to avo2 uuid's
 CREATE TABLE reference_user_ids
 (external_uid int primary key,
-user_id varchar(36));
+uid varchar(36));
 
 
 -- INSERT values from AVO2.shared.users
