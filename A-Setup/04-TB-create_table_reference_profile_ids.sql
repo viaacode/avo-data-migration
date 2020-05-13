e7d1e8c841cd62db756044d55b1de741e4a6bee2
Bart Debunne <bart.debunne@viaa.be>
2020-05-13 07:43:48 +0200

    Use meaningful field name in avo1 reference_profile_ids.

--- A-Setup/04-TB-create_table_reference_profile_ids.sql
+++ A-Setup/04-TB-create_table_reference_profile_ids.sql
@@ -3,10 +3,7 @@
 -- Create table for future mapping of Drupal profiles to avo2 profiles
 CREATE TABLE reference_profile_ids (
   user_id varchar(36) primary key,
-  profile_id varchar(36),
-  pid int
+  profile_id varchar(36)
 );
 
-
 -- INSERT values (external_uid,profile_id) from AVO2.shared.users
--- ISERT values (pid) from TESTBEELD.profile JOIN ON external_uid
