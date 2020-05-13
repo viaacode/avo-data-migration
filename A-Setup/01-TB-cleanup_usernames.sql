c30b24567ebe52edcf938a1514172e1405f7d0c9
Bart Debunne <bart.debunne@viaa.be>
2020-05-13 07:41:19 +0200

    Added line to strip username suffixes (e. John Smith1) from usernames.

--- A-Setup/01-TB-cleanup_usernames.sql
+++ A-Setup/01-TB-cleanup_usernames.sql
@@ -3,8 +3,9 @@
 -- Drop index on name so we can clean-up values
 ALTER TABLE users drop index name;
 
--- Remove illicit tabs from usernames
+-- Remove illicit tabs and numbers from usernames
 UPDATE users SET name = REPLACE(name, '\t', ' ' );
+UPDATE users SET name = IF(name REGEXP '[[:digit:]]+' = 1, SUBSTRING(name, 1, CHAR_LENGTH(name)-1), name);
 
 -- Recreate index on name
 CREATE INDEX name ON users (name);
